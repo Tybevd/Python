59e3ed168de07fc79c22521739d80a59886f5366
Tybevd <114216953+Tybevd@users.noreply.github.com>
2023-09-03 01:12:06 +0700

    Update and rename test.py to ICPC0118 - CUNG HOÀNG ĐẠO.py

--- ICPC0118 - CUNG HOÀNG ĐẠO.py
+++ ICPC0118 - CUNG HOÀNG ĐẠO.py
@@ -18,9 +18,9 @@ def correctZodiac(a):
         return 'Thien Yet'
     if (int(a[1])==11 and int(a[0])>=23) or (int(a[1])==12 and int(a[0])<=21):
         return 'Nhan Ma'
-    if (int(a[1])==12 and int(a[0])>=22) or (int(a[1])==1 and int(a[0])>=19):
+    if (int(a[1])==12 and int(a[0])>=22) or (int(a[1])==1 and int(a[0])<=19):
         return 'Ma Ket'
-    if (int(a[1])==1 and int(a[0])>=20) or (int(a[1])==2 and int(a[0])>=18):
+    if (int(a[1])==1 and int(a[0])>=20) or (int(a[1])==2 and int(a[0])<=18):
         return 'Bao Binh'
     if (int(a[1])==2 and int(a[0])>=19) or (int(a[1])==3 and int(a[0])<=20):
         return 'Song Ngu'
